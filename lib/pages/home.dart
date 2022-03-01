d53527e91c315aabaa49f76237ba1bfbd0b6455e
Jongwoo Lim <jwlim94@Jongwoos-MBP.app.byu.edu>
2022-02-28 17:55:05 -0700

    complete setting size and position of the logo at home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return Scaffold(
+      body: Container(
+        // width: MediaQuery.of(context).size.width,
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          // border: Border.all(color: Colors.white),
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/images/black-rocks.jpeg",
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 100.0),
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.white),
+              // ),
+            ),
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Image.asset(
+                "assets/images/neighbor-name-and-logo.png",
+                width: MediaQuery.of(context).size.width / 2,
+              ),
+              alignment: Alignment.topCenter,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
